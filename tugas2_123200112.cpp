cc10cc76249a5cbd3df368e0609e7c41a7255f65
Dwikaputra11 <dwikariswanda@gmail.com>
2020-10-21 23:18:54 +0800

    Tugas2 Praktikum Algoritma

--- tugas2_123200112.cpp
+++ tugas2_123200112.cpp
@@ -12,38 +12,39 @@ int main()
 	cin >> umurayah;
 	if (umurayah > 20){
 		d1 = 30000;
-	} else if (umurayah >= 12 && umurayah <= 20){
+	} else if (umurayah > 12 && umurayah <= 20){
 		d1 = 20000;
-	} else {cout << "Umur tidak bisa dibaca" << endl;}
+	} 
 
 	cout << "umur Ibu 	: ";
 	cin >> umuribu;
 	if (umuribu > 20){
 		d2 = 30000;
-	} else if (umuribu >= 12 && umuribu <= 20){
+	} else if (umuribu > 12 && umuribu <= 20){
 		d2 = 20000;
-	} else {cout << "Umur tidak bisa dibaca" << endl;}
+	}
 
 
 	cout << "Umur Anak 1 	: ";
 	cin >> umuranak1;
 	if (umuranak1 > 20){
 		r = 30000;
-	} else if (umuranak1 >= 12 && umuranak1 <= 20){
+	} else if (umuranak1 > 12 && umuranak1 <= 20){
 		r = 20000;
-	} else {cout << "Umur tidak bisa dibaca" << endl;}
+	} else if (umuranak1 <= 12){
+		r = 15000;
+	}
 
 
 	cout << "Umur Anak 2 	: ";
 	cin >> umuranak2;
 	if (umuranak2 > 20){
 		a = 30000;
-	} else if (umuranak2 >= 12 && umuranak2 <= 20){
+	} else if (umuranak2 > 12 && umuranak2 <= 20){
 		a = 20000;
-	} else if (umuranak2 < 12){
+	} else if (umuranak2 <= 12){
 		a = 15000;
 	} 
-	else {cout << "Umur tidak bisa dibaca" << endl;}
 
 
 	total = 30*(d1 + d2 + r + a);
